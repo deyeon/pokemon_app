6191fc262dc19d6a86b6fec0731948aa5a1d5567
deyeon <deyeon209@gmail.com>
2022-12-19 11:45:23 +0900

    19일 수정

--- app_eda.py
+++ app_eda.py
@@ -10,13 +10,10 @@ def run_eda_app():
     df=df.drop(df.columns[0],axis=1)
     df=df.dropna(axis=1)
     
-    st.subheader('통계 데이터 확인')
-    st.dataframe(df.head(3))
-    
-    st.subheader('기본 통계 데이터')
+    st.subheader('데이터 요약')
     st.dataframe(df.describe())
 
-    st.subheader('최대/최소 데이터 확인하기')
+    st.subheader('포켓몬 별 최대/최소 데이터 확인하기')
     column_list = df.columns[8:]
 
     selected_column=st.selectbox('컬럼을 선택하세요',column_list)
@@ -34,17 +31,39 @@ def run_eda_app():
     df2=df.loc[:,['generation','status','type_1']]
 
     column_list2 = df2.columns
-    histogram_coulmn=st.selectbox('히스토그램 확인 할 컬럼을 선택하세요',column_list2)
+    status1 =st.radio('설정을 선택하세요',['세대별','등급별','타입별'])
 
     import plotly.express as px
+    if status1 == '세대별' :
+        st.header('세대별 히스토그램')
+        fig1=px.histogram(df2,x=df2['generation'],nbins=20,color_discrete_sequence=["green"])
+        fig1.update_layout(bargap=0.2,plot_bgcolor='#FFFFFF')
+        st.plotly_chart(fig1)
+        
+        st.subheader('파이 차트')
+        fig2 = px.pie(df2,names=df2['generation'].value_counts().index,values=df2['generation'].value_counts(),title='파이차트')
+        st.plotly_chart(fig2)
+    
+    elif status1 == '등급별' :
+        st.header('등급별 히스토그램')
+        fig3=px.histogram(df2,x=df2['status'],nbins=20,color_discrete_sequence=['blue'])
+        fig3.update_layout(bargap=0.2,plot_bgcolor='#FFFFFF')
+        st.plotly_chart(fig3)
+        
+        st.subheader('파이 차트')
+        fig4 = px.pie(df2,names=df2['status'].value_counts().index,values=df2['status'].value_counts(),title='파이차트')
+        st.plotly_chart(fig4)
 
-    fig=px.histogram(df2,x=histogram_coulmn,nbins=20)
-    fig.update_layout(bargap=0.2)
-    st.plotly_chart(fig)
-
-    st.subheader('파이 차트')
-    fig1 = px.pie(df2,names=df2[histogram_coulmn].value_counts().index,values=df2[histogram_coulmn].value_counts(),title='파이차트')
-    st.plotly_chart(fig1)
+    elif status1 == '타입별' :
+        st.header('타입별 히스토그램')
+        fig5=px.histogram(df2,x=df2['type_1'],nbins=20,color_discrete_sequence=['red'])
+        fig5.update_layout(bargap=0.2,plot_bgcolor='#FFFFFF')
+        st.plotly_chart(fig5)
+        
+        st.subheader('파이 차트')
+        fig6 = px.pie(df2,names=df2['type_1'].value_counts().index,values=df2['type_1'].value_counts(),title='파이차트')
+        st.plotly_chart(fig6)
+    
 
     
 
